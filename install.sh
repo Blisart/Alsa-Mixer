5f02e835d092ed44c6d55e24814eb26ffa95d9b4
Blisart <web.klute@gmail.com>
2022-11-08 00:22:57 +0300

    Change install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Clonning extension..."
-git clone git://github.com/Blisart/Alsa-Mixer.git Alsa-Mixer
+git clone https://github.com/Blisart/Alsa-Mixer.git Alsa-Mixer
 echo "Creating extension folder..."
-mkdir -p ~/.local/share/gnome-shell/extensions/Alsa-Mixer
+mkdir -p ~/.local/share/gnome-shell/extensions/Alsa-Mixer\@Blisart
 echo "Installing extension..."
-cp Alsa-Mixer/extension.js ~/.local/share/gnome-shell/extensions/Alsa-Mixer
-cp Alsa-Mixer/metadata.json ~/.local/share/gnome-shell/extensions/Alsa-Mixer
+cp Alsa-Mixer/extension.js ~/.local/share/gnome-shell/extensions/Alsa-Mixer\@Blisart/
+cp Alsa-Mixer/metadata.json ~/.local/share/gnome-shell/extensions/Alsa-Mixer\@Blisart/
 echo "Cleaning..."
 rm -rf Alsa-Mixer/
 echo "Done!"
