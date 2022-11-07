8d563ffd153b009659dbd9a5badd407551de0372
Blisart <web.klute@gmail.com>
2022-11-07 23:58:58 +0300

    Init

--- extension.js
+++ extension.js
@@ -1,161 +1,66 @@
-/* -*- coding: UTF-8; mode: js2; js2-basic-offset: 4 -*- */
-/*
- * Copyright (C) 2012, 2013 Victor Aurélio Santos <victoraur.santos@gmail.com>
- * 
- * This file is part of Alsa-Mixer.
- *
- * Alsa Mixer is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Alsa Mixer is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 const St = imports.gi.St;
-const Lang = imports.lang;
+const Gio = imports.gi.Gio;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Clutter = imports.gi.Clutter;
 const GLib = imports.gi.GLib;
-const Util = imports.misc.util;
+const ByteArray = imports.byteArray;
 const Mainloop = imports.mainloop;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
 
-// Step on scroll, and olny on scroll
-const VOLUME_STEP = 5; 
-// Mixer element to control, for example 'Master Surround'
-const MIXER_ELEMENT = 'Master';
+const SPEAKER_ICON = "sp.svg";
+const HEADPHONE_ICON = "hp.svg";
 
-const AlsaMixer = new Lang.Class({
-    Name: 'AlsaMixer',
-    Extends: PanelMenu.SystemStatusButton,
-    
-    _init: function() {
-        this.parent('audio-volume-medium', _('Volume'));
-        
-        this.statusIcon = new St.Icon({
-            icon_name: 'audio-volume-medium',
-            style_class: 'status-icon'
-        });
-        this.actor.add_actor(this.statusIcon);
-        
-        this._onScrollId = this.actor.connect('scroll-event',
-            Lang.bind(this, this._onScroll));
-        
-        this._cVolume = this._getVolume();
-        this._muted = this._cVolume < 1 ? true : false;
-        this._updateIcon(this._cVolume);
-        
-        this.pup = new PopupMenu.PopupSliderMenuItem(this._cVolume / 100);
-        this._onSliderId = this.pup.connect('value-changed',
-                Lang.bind(this, this._onSlider));
-        this.menu.addMenuItem(this.pup);
-        
-        this._timeoutId = Mainloop.timeout_add_seconds(1,
-                Lang.bind(this, this._onUpdate));
-    },
-    
-    _getVolume: function() {
-        let cmd = GLib.spawn_command_line_sync(
-                'env LANG=C amixer get %s'.format(MIXER_ELEMENT));
-        let re = /\[(\d{1,2})\%\]/m;
-        let values = re.exec(cmd[1]);
+// amixer -c 2 set 'Output Select' 'Speakers'
+const OUTPUT_ITEMS = {
+    0: 'Speakers',
+    1: 'Headphone'
+}
+const MIXER_ELEMENT = 'Output Select';
 
-        return values[1];
-    },
-    
-    _setVolume: function(value) {
-        let cmd = GLib.spawn_command_line_async(
-                'amixer -q set %s %s %%'.format(MIXER_ELEMENT, value));
-        
-        this._cVolume = value;
-        
-        this._updateIcon(value);
-    },
-    
-    _updateIcon: function(value) {
-        if (this.statusIcon.get_icon_name() != this._getIcon(value)) {
-            let icon = this._getIcon(value);
-            this.statusIcon.set_icon_name(icon);
-            this.setIcon(icon);
-        }
-    },
-    
-    _getIcon: function(volume) {
-        let rvalue = 'audio-volume-muted';
-        if (volume < 1) {
-            rvalue = 'audio-volume-muted';
-        } else {
-            let num = Math.floor(3 * volume / 100) + 1;
+let button, icon, gicon_speakers, gicon_headphone;
 
-            if (num >= 3)
-                rvalue = 'audio-volume-high';
-            else if(num < 2)
-                rvalue = 'audio-volume-low';
-            else
-                rvalue = 'audio-volume-medium';
-        }
-        return rvalue;
-    },
-    
-    _onScroll: function(widget, event) {
-        let di = event.get_scroll_direction();
-        
-        if (di == Clutter.ScrollDirection.DOWN
-                && Number(this._cVolume) > VOLUME_STEP) {
-            this._setVolume(Number(this._cVolume) - VOLUME_STEP);
-        } else if (di == Clutter.ScrollDirection.DOWN
-                && Number(this._cVolume) <= VOLUME_STEP) {
-            this._setVolume(0);
-        } else if(di == Clutter.ScrollDirection.UP
-                && Number(this._cVolume) < 100 - VOLUME_STEP) {
-            this._setVolume(Number(this._cVolume) + VOLUME_STEP);
-        } else if(di == Clutter.ScrollDirection.UP
-                && Number(this._cVolume) >= 100 - VOLUME_STEP) {
-            this._setVolume(100);
-        }
-        this.pup.setValue(Number(this._cVolume) / 100);
-    },
-    
-    _onSlider: function(slider, value) {
-        let volume = value * 100;
-        this._setVolume(volume);
-    },
-    
-    _onUpdate: function() {
-        this._cVolume = this._getVolume();
-        this._updateIcon(this._cVolume);
-        this.pup.setValue(Number(this._cVolume) / 100);
-        return true;
-    },
-    
-    destroy: function() {
-        this.parent();
-        Mainloop.source_remove(this._timeoutId);
-        this.actor.disconnect(this._onScrollId);
-        this.pup.disconnect(this._onSliderId);
-    },
-});
-
-function init() {
-    // pass
+function init(metadata) {
 }
 
-var AM;
+function toggleOutput() {
+    let [ok, out, err, exit] = GLib.spawn_command_line_sync("amixer -c 2 get '" + MIXER_ELEMENT + "'");
+
+    if (ByteArray.toString(out).includes('Item0: \'Headphone\'')) {
+        icon.set_gicon(gicon_headphone)
+    } else {
+        icon.set_gicon(gicon_speakers)
+    }
+}
+
 
 function enable() {
-    AM = new AlsaMixer();
-    Main.panel.addToStatusArea('AlsaMixer', AM);
+    button = new PanelMenu.Button(0.0);
+    icon = new St.Icon({ style_class: 'system-status-icon' });
+    gicon_speakers = Gio.icon_new_for_string(Me.path + "/icons/" + SPEAKER_ICON);
+    gicon_headphone = Gio.icon_new_for_string(Me.path + "/icons/" + HEADPHONE_ICON);
+
+    let [ok, out, err, exit] = GLib.spawn_command_line_sync("sh -c 'cat /sys/bus/hid/devices/*17EF\:604*/fn_lock'");
+
+    if (ByteArray.toString(out).includes('Item0: \'Headphone\'')) {
+        GLib.spawn_command_line_sync("amixer -c 2 set '" + MIXER_ELEMENT + "' '" + OUTPUT_ITEMS["1"] + "'")
+        icon.set_gicon(gicon_headphone)
+    } else {
+        GLib.spawn_command_line_sync("amixer -c 2 set '" + MIXER_ELEMENT + "' '" + OUTPUT_ITEMS["0"] + "'")
+        icon.set_gicon(gicon_speakers)
+    }
+
+    button.actor.add_actor(icon);
+    button.actor.connect('button-press-event', toggleOutput);
+    Main.panel.addToStatusArea('OutputDevice', button);
 }
 
+
 function disable() {
-    AM.destroy();
-    AM = null;
+    button.destroy();
+    button = null;
+    icon = null;
+    gicon_speakers = null;
+    gicon_headphone = null;
 }
+
